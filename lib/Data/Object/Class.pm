61c794586c4e8ea34114dc7408d5e140b96ab879
Al Newkirk <anewkirk@ana.io>
2015-03-31 17:03:22 -0400

    Added partially implemented base Class and Role modules.

---
+++ lib/Data/Object/Class.pm
@@ -0,0 +1,9 @@
+# A Generic Class for Perl 5
+package Data::Object::Class;
+
+use 5.010;
+use parent 'Moo';
+
+# VERSION
+
+1;
---
+++ lib/Data/Object/Role.pm
@@ -0,0 +1,9 @@
+# A Generic Role for Perl 5
+package Data::Object::Role;
+
+use 5.010;
+use parent 'Moo::Role';
+
+# VERSION
+
+1;
